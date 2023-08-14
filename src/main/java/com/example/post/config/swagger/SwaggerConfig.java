654b1c04624b4a3bc573b7361058b678336ca231
bohyunsung <bohyunsung@smartscore.kr>
2023-08-14 13:37:03 +0900

    swagger ignored 추가

--- src/main/java/com/example/post/config/swagger/SwaggerConfig.java
+++ src/main/java/com/example/post/config/swagger/SwaggerConfig.java
@@ -12,11 +12,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.AlternateTypeRule;
 import springfox.documentation.schema.AlternateTypeRules;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
@@ -33,6 +32,7 @@ public class SwaggerConfig {
     public Docket api() {
         TypeResolver typeResolver = new TypeResolver();
         return new Docket(DocumentationType.OAS_30)
+                .ignoredParameterTypes(AuthenticationPrincipal.class)
                 .alternateTypeRules(AlternateTypeRules.newRule(
                         typeResolver.resolve(Pageable.class),
                         typeResolver.resolve(RequestPageableModel.class)))
